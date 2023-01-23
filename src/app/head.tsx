2f37bccc644ae0d422b80c17f9dc149b9a2c61f9
Kelvin Ampofo <kelvinamp@outlook.com>
2023-01-23 17:25:27 +0000

    add meta tags (seo)

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -2,6 +2,19 @@ export default function Head() {
   return (
     <>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="author" content="Kelvin Ampofo" />
+      <meta name="title" content="Kelvin Ampofo" />
+      <meta name="twitter:title" content="Kelvin" />
+      <meta name="twitter:site" content="@kelvinamp_" />
+      <meta
+        property="og:description"
+        content="Crafting interfaces. Code, design, human-computer interaction and architecture."
+      />
+      <meta property="og:type" content="website" />
+      <meta property="og:site_name" content="Kelvin Ampofo" />
+      <meta property="og:url" content="https://kelvinamp.me" />
+      <meta property="og:title" content="Kelvin Ampofo" />
+      <link rel="canonical" href="https://kelvinamp.me" />
       <link rel="icon" href="/favicon.ico" />
       <title>Kelvin Ampofo</title>
     </>
