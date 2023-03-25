cb444212757a13ae97afd24f556c567aa3239d95
Kelvin Ampofo <kelvinamp@outlook.com>
2023-03-25 12:30:14 +0000

    simplify

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -17,25 +17,23 @@ export default function Contact() {
   return (
     <section>
       <h2 className="mb-6 font-medium dark:text-white">Connect</h2>
-      <nav>
-        <ul className="flex">
-          {links.map((link, index) => (
-            <li key={index} className="flex items-center">
-              <a
-                href={link.href}
-                className="underline decoration-neutral-500 decoration-1 underline-offset-4 duration-500 ease-in-out hover:text-gray-500"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {link.label}
-              </a>
-              {index !== links.length - 1 && (
-                <span className="mx-1">&middot;</span>
-              )}
-            </li>
-          ))}
-        </ul>
-      </nav>
+      <ul className="flex">
+        {links.map((link, index) => (
+          <li key={index} className="flex items-center">
+            <a
+              href={link.href}
+              className="underline decoration-neutral-500 decoration-1 underline-offset-4 duration-500 ease-in-out hover:text-gray-500"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {link.label}
+            </a>
+            {index !== links.length - 1 && (
+              <span className="mx-1">&middot;</span>
+            )}
+          </li>
+        ))}
+      </ul>
     </section>
   );
 }
