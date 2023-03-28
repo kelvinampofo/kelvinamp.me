cbb2cdf11b3652d5f26faf984f488ecfd408753c
Kelvin Ampofo <kelvinamp@outlook.com>
2023-03-28 15:50:05 +0100

    a11y update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="absolute bottom-0 mx-auto mb-3 flex w-full max-w-3xl flex-col items-end px-8 text-xs dark:text-neutral-400 md:text-sm">
       <span>
-        <i>{new Date().getFullYear()}</i> © Kelvin Ampofo.
+        <i>{new Date().getFullYear()}</i> &copy; Kelvin Ampofo.
       </span>
     </footer>
   );
