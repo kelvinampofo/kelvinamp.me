c119d80d662ebeee4808b37e9337d2d0c5b795fa
Kelvin Ampofo <kelvinamp@outlook.com>
2023-03-28 17:52:58 +0100

    update og image size

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,8 +27,8 @@ export const metadata: Metadata = {
     images: [
       {
         url: 'https://kelvinamp.me/og.jpeg',
-        width: 1920,
-        height: 1080
+        width: 1200,
+        height: 630
       }
     ]
   },
