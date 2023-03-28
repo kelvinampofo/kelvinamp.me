230b0dfa36e3406ac12a4ee29967c2a0fb3b7d0e
Kelvin Ampofo <kelvinamp@outlook.com>
2023-03-28 17:07:50 +0100

    update intro

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,21 @@ import Contact from '@/components/Contact';
 
 export default function About() {
   return (
-    <section className="flex flex-col justify-center px-8 pb-32 pt-20 md:pt-32 md:pb-36 lg:px-10 lg:py-36">
-      <h1 className="text-xl font-medium md:text-[22px]">Kelvin Ampofo</h1>
-      <h2 className="mb-6 dark:text-neutral-400">Software engineer</h2>
-      <article>
+    <section className="flex flex-col justify-center px-8 py-24 lg:px-10">
+      <h1 className="text-xl font-medium dark:text-white">Kelvin Ampofo</h1>
+      <span className="dark:text-neutral-400">Software Engineer</span>
+      <section className="mt-6">
         <p>
           <em>Crafting interfaces</em> with a focus on design, human-computer
           interaction and architecture. Currently working in Fintech.
         </p>
-      </article>
-      <hr className="my-12 h-px border-0 bg-black dark:bg-[#292C2D]" />
+        <br />
+        <p>
+          Committed to developing craftmanship—guiltlessly exploring art and
+          technology.
+        </p>
+      </section>
+      <hr className="my-12 h-px border-0 bg-black dark:bg-neutral-800" />
       <Contact />
     </section>
   );
