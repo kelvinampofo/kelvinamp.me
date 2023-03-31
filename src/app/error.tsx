e9f68056ef878a4abb89e54bc51c4305c22ae27a
Kelvin Ampofo <kelvinamp@outlook.com>
2023-03-31 17:00:17 +0100

    update error.tsx

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -4,13 +4,15 @@ import { useEffect } from 'react';
 
 export default function Error({ error }: { error: Error; reset: () => void }) {
   useEffect(() => {
-    // Log the error to an error reporting service
+    // TODO: Log the error to an error reporting service.
     console.error(error);
   }, [error]);
 
   return (
     <div>
-      <p>Oops, something went wrong... very sad. Maybe refresh?</p>
+      <p className="mt-44">
+        Oops, something went wrong... very much sad. Maybe refresh?
+      </p>
     </div>
   );
 }
