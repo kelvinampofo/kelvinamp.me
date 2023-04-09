c6a83175739b9a0af07bb375c1990f4a3ce12a4c
Kelvin Ampofo <kelvinamp@outlook.com>
2023-04-09 14:26:26 +0100

    remove navigation

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,5 @@
 import Container from '@/components/Container';
-import { ArrowTopLeftIcon } from '@radix-ui/react-icons';
 import { Metadata } from 'next';
-import Link from 'next/link';
 
 export const metadata: Metadata = {
   title: '404 – Not Found'
@@ -19,19 +17,6 @@ export default function NotFound() {
         </p>
         <p>Either way, apologies for the inconvenience.</p>
       </section>
-      <span className="mt-6">
-        <Link
-          href="/"
-          className="inline-flex items-center underline decoration-neutral-300 decoration-1 underline-offset-4 duration-500 ease-in-out hover:text-neutral-500 dark:decoration-neutral-500 dark:hover:text-neutral-400"
-          aria-label="Go to home page"
-        >
-          <ArrowTopLeftIcon
-            className="mr-1 text-neutral-500 dark:text-neutral-400"
-            aria-label="Top left arrow icon"
-          />
-          Index
-        </Link>
-      </span>
     </Container>
   );
 }
