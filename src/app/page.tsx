b4f309a713a8ac68ef8744de771e5971ef16a990
Kelvin Ampofo <kelvinamp@outlook.com>
2023-04-10 22:09:19 +0100

    add page wrapper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,43 @@
 import Contact from '@/components/Contact';
 import Container from '@/components/Container';
+import PageWrapper from '@/components/PageWrapper';
 import { ArrowTopRightIcon } from '@radix-ui/react-icons';
 import Link from 'next/link';
 
 export default function Home() {
   return (
-    <Container>
-      <h1 className="text-xl font-medium dark:text-white">Kelvin Ampofo</h1>
-      <span className="mb-6 text-neutral-500 dark:text-neutral-400">
-        Software Engineer
-      </span>
-      <section>
-        <p className="mb-6">
-          <em>Crafting interfaces</em> with a focus on design, human-computer
-          interaction and architecture. Currently working in Fintech.
-        </p>
-        <p>
-          Developing skill through immersive, hands-on exploration in art and
-          technology.
-        </p>
-      </section>
-      <span>
-        <Link
-          href="/now"
-          className="mt-6 inline-flex items-center underline decoration-neutral-300 decoration-1 underline-offset-4 duration-500 ease-in-out hover:text-neutral-500 dark:decoration-neutral-500 dark:hover:text-neutral-400"
-          aria-label="Go to now page"
-        >
-          Now
-          <ArrowTopRightIcon
-            className="ml-1 text-neutral-500 dark:text-neutral-400"
-            aria-label="Top right arrow icon"
-          />
-        </Link>
-      </span>
-      <hr className="my-12 h-px border-0 bg-neutral-200 dark:bg-neutral-800" />
-      <Contact />
-    </Container>
+    <PageWrapper>
+      <Container>
+        <h1 className="text-xl font-medium dark:text-white">Kelvin Ampofo</h1>
+        <span className="mb-6 text-neutral-500 dark:text-neutral-400">
+          Software Engineer
+        </span>
+        <section>
+          <p className="mb-6">
+            <em>Crafting interfaces</em> with a focus on design, human-computer
+            interaction and architecture. Currently working in Fintech.
+          </p>
+          <p>
+            Developing skill through immersive, hands-on exploration in art and
+            technology.
+          </p>
+        </section>
+        <span>
+          <Link
+            href="/now"
+            className="mt-6 inline-flex items-center underline decoration-neutral-300 decoration-1 underline-offset-4 duration-500 ease-in-out hover:text-neutral-500 dark:decoration-neutral-500 dark:hover:text-neutral-400"
+            aria-label="Go to now page"
+          >
+            Now
+            <ArrowTopRightIcon
+              className="ml-1 text-neutral-500 dark:text-neutral-400"
+              aria-label="Top right arrow icon"
+            />
+          </Link>
+        </span>
+        <hr className="my-12 h-px border-0 bg-neutral-200 dark:bg-neutral-800" />
+        <Contact />
+      </Container>
+    </PageWrapper>
   );
 }
---
+++ src/components/PageWrapper.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import { motion } from 'framer-motion';
+
+export default function AnimateWrapper({
+  children,
+  className
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) {
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: 20 }}
+      transition={{ ease: 'easeIn', duration: 0.5 }}
+      className={className}
+    >
+      {children}
+    </motion.div>
+  );
+}
