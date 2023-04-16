e1fd8f10f1b901267566cd2c73949b739c4a1b6a
Kelvin Ampofo <kelvinamp@outlook.com>
2023-04-16 11:06:38 +0100

    delay prop

--- src/components/PageWrapper.tsx
+++ src/components/PageWrapper.tsx
@@ -2,19 +2,23 @@
 
 import { motion } from 'framer-motion';
 
-export default function PageWrapper({
-  children,
-  className
-}: {
+interface PageWrapperProps {
   children: React.ReactNode;
   className?: string;
-}) {
+  delay?: number;
+}
+
+export default function PageWrapper({
+  children,
+  className,
+  delay = 0
+}: PageWrapperProps) {
   return (
     <motion.div
       initial={{ opacity: 0, y: 20 }}
       animate={{ opacity: 1, y: 0 }}
       exit={{ opacity: 0, y: 20 }}
-      transition={{ ease: 'linear', duration: 0.35 }}
+      transition={{ ease: 'linear', duration: 0.4, delay }}
       className={className}
     >
       {children}
