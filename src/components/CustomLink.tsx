26507067dd6323fedb852bdb1227220663b2a633
Kelvin Ampofo <kelvinamp@outlook.com>
2023-04-30 21:08:50 +0100

    small updates

--- src/components/CustomLink.tsx
+++ src/components/CustomLink.tsx
@@ -26,15 +26,15 @@ export default function CustomLink({
         className={clsx(
           arrowIcon ? 'inline-flex items-center' : '',
           underline
-            ? 'underline decoration-[#6F6F6F] decoration-1 underline-offset-4 dark:decoration-neutral-400'
+            ? 'underline decoration-[#6F6F6F] decoration-1 underline-offset-4 dark:decoration-neutral-500'
             : '',
-          'duration-500 ease-linear hover:text-[#6F6F6F]  dark:hover:text-neutral-400'
+          'duration-500 ease-linear hover:text-[#6F6F6F] dark:hover:text-neutral-400'
         )}
         aria-label={ariaLabel}
       >
         {arrowIcon && (
           <ArrowTopLeftIcon
-            className="mr-[3px]"
+            className="mr-[3px] text-[#6F6F6F] dark:text-neutral-400"
             aria-label="ArrowTopLeft icon"
             width={18}
             height={18}
@@ -48,7 +48,7 @@ export default function CustomLink({
   return (
     <a
       href={href}
-      className="underline decoration-neutral-400 decoration-1 underline-offset-4 duration-500 ease-in-out hover:text-neutral-500 dark:decoration-neutral-500 dark:hover:text-neutral-400"
+      className="underline decoration-[#6F6F6F] decoration-1 underline-offset-4 duration-500 ease-linear hover:text-[#6F6F6F] dark:decoration-neutral-500 dark:hover:text-neutral-400"
       target="_blank"
       rel="noopener noreferrer"
       aria-label={ariaLabel}
