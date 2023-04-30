69ba0dd92d6f839bb00cd85c207344b57508420c
Kelvin Ampofo <kelvinamp@outlook.com>
2023-04-30 21:13:40 +0100

    update delay

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,17 +6,17 @@ import PageWrapper from '@/components/PageWrapper';
 export default function Home() {
   return (
     <Container>
-      <PageWrapper delay={0.6}>
+      <PageWrapper delay={0.4}>
         <h1 className="text-lg font-medium">Kelvin Ampofo</h1>
         <span className="text-[#6F6F6F] dark:text-neutral-400">
           Software Engineer
         </span>
       </PageWrapper>
-      <PageWrapper delay={0.8}>
+      <PageWrapper delay={0.6}>
         <section className="my-6 flex flex-col gap-6">
           <p>
             <em>Crafting interfaces</em> with a focus on design, human-computer
-            interaction and architecture. Currently working in Fintech.
+            interaction and architecture. Currently working in digital banking.
           </p>
           <p>
             Developing skill through immersive, hands-on exploration in art and
@@ -37,7 +37,7 @@ export default function Home() {
         </span>
         <hr className="my-12 h-px border-0 bg-neutral-200 dark:bg-neutral-800" />
       </PageWrapper>
-      <PageWrapper delay={1.0}>
+      <PageWrapper delay={0.8}>
         <Contact />
       </PageWrapper>
     </Container>
