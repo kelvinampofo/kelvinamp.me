48e63244daf878a8bf39d294950b92b727f8c288
Kelvin Ampofo <kelvinamp@outlook.com>
2023-07-01 16:27:05 +0100

    feat(tooltip): add animiations

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,15 @@ export default {
     extend: {
       fontFamily: {
         sans: ['var(--font-inter)']
+      },
+      animation: {
+        tooltip: 'tooltipAnimation 0.25s ease-in-out'
+      },
+      keyframes: {
+        tooltipAnimation: {
+          '0%': { transform: 'translateY(10px)', opacity: '0' },
+          '100%': { transform: 'translateY(0)', opacity: '1' }
+        }
       }
     }
   },
