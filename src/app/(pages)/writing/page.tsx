99af6ec2eb8e37ea923642739d061c4dc08f1c91
Kelvin Ampofo <kelvinamp@outlook.com>
2023-08-13 19:58:34 +0100

    feat: add List component

--- src/app/(pages)/writing/page.tsx
+++ src/app/(pages)/writing/page.tsx
@@ -1,8 +1,8 @@
 import Container from '@/app/components/generic/Container';
+import Seperator from '@/app/components/generic/Seperator';
 import CustomLink from '@/app/components/ui/CustomLink';
-import { isWithin2Months } from '@/app/lib/utils';
+import PostList from '@/app/components/ui/List';
 import { allPosts } from 'contentlayer/generated';
-import { compareDesc, format, parseISO } from 'date-fns';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
@@ -33,36 +33,12 @@ export const metadata: Metadata = {
 export default function Writing() {
   return (
     <Container>
-      <h1 className="mb-6 text-lg font-medium">Writing</h1>
+      <h1 className="mb-3 text-lg font-medium">Writing</h1>
       <p className="mb-12 text-[#6F6F6F] dark:text-neutral-400">
         Infrequent thoughts on design, technology and things in between.
       </p>
-      <hr className="mb-3 h-px border-0 bg-neutral-200 dark:bg-neutral-800" />
-      {allPosts
-        .sort((a, b) => compareDesc(new Date(a.publishedAt), new Date(b.publishedAt)))
-        .map((post) => {
-          const postDate = parseISO(post.publishedAt);
-          return (
-            <>
-              <CustomLink href={`/writing/${post.slug}`} key={post._id}>
-                <div className="flex justify-between">
-                  <span className="flex font-medium">
-                    {post.title}{' '}
-                    {isWithin2Months(postDate) && (
-                      <span className="ml-2 animate-shine items-baseline bg-gradient-to-r from-teal-500 via-blue-600 to-teal-500 bg-200 bg-clip-text bg-left text-xs text-transparent dark:from-teal-200 dark:via-blue-600 dark:to-teal-200">
-                        new
-                      </span>
-                    )}
-                  </span>
-                  <time className="text-[#6F6F6F] dark:text-neutral-400">
-                    {format(postDate, 'dd/MM/yy')}
-                  </time>
-                </div>
-              </CustomLink>
-              <hr className="my-3 h-px border-0 bg-neutral-200 dark:bg-neutral-800" />
-            </>
-          );
-        })}
+      <Seperator className="mb-3" />
+      <PostList items={allPosts} />
       <span className="mt-12">
         <CustomLink href="/" ariaLabel="go back to home page" arrowIcon>
           Back
---
+++ src/app/components/ui/List.tsx
@@ -0,0 +1,45 @@
+import type { Project } from '@/app/data/data';
+import { isWithin2Months } from '@/app/lib/utils';
+import type { Post } from 'contentlayer/generated';
+import { compareDesc, format, parseISO } from 'date-fns';
+import { Fragment } from 'react';
+import Seperator from '../generic/Seperator';
+import CustomLink from './CustomLink';
+
+interface ContentListProps {
+  items: Project[] | Post[];
+}
+
+export default function List({ items }: ContentListProps) {
+  return (
+    <>
+      {items
+        .sort((a, b) => compareDesc(parseISO(a.publishedAt), parseISO(b.publishedAt)))
+        .map((content) => {
+          const publishedDate = parseISO(content.publishedAt);
+
+          const isNewContent = isWithin2Months(publishedDate);
+          return (
+            <Fragment key={content._id}>
+              <CustomLink href={content.slug}>
+                <div className="flex justify-between">
+                  <span className="flex font-medium">
+                    {content.title}
+                    {isNewContent && (
+                      <span className="ml-2 animate-shine items-baseline bg-gradient-to-r from-teal-500 via-blue-600 to-teal-500 bg-200 bg-clip-text bg-left text-xs text-transparent dark:from-teal-200 dark:via-blue-600 dark:to-teal-200">
+                        new
+                      </span>
+                    )}
+                  </span>
+                  <time className="text-[#6F6F6F] dark:text-neutral-400">
+                    {format(publishedDate, 'dd/MM/yy')}
+                  </time>
+                </div>
+              </CustomLink>
+              <Seperator className="my-3" />
+            </Fragment>
+          );
+        })}
+    </>
+  );
+}
