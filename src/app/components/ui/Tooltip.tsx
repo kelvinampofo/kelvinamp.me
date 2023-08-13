5f7fac310f2a0633e3eb70b887465c05556a3f33
Kelvin Ampofo <kelvinamp@outlook.com>
2023-08-13 17:57:33 +0100

    chore: name change

--- src/app/components/ui/Tooltip.tsx
+++ src/app/components/ui/Tooltip.tsx
@@ -11,7 +11,7 @@ export default function Tooltip({ children, content }: Props) {
       <TooltipPrimitive.Root delayDuration={350}>
         <TooltipPrimitive.Trigger asChild>{children}</TooltipPrimitive.Trigger>
         <TooltipPrimitive.Content
-          className="animate-tooltip rounded-md border p-1 text-xs shadow-md duration-200 dark:border-neutral-800 dark:shadow-neutral-950"
+          className="animate-slideUp rounded-md border p-1 text-xs shadow-md duration-200 dark:border-neutral-800 dark:shadow-neutral-950"
           sideOffset={2}
         >
           {content}
