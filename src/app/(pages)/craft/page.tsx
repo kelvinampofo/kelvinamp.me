9a7330724b8db38d0a8f7298caebbda2a46e3b46
Kelvin Ampofo <kelvinamp@outlook.com>
2023-08-14 22:14:26 +0100

    chore: add author to craft page

--- src/app/(pages)/craft/page.tsx
+++ src/app/(pages)/craft/page.tsx
@@ -9,6 +9,7 @@ import React from 'react';
 export const metadata: Metadata = {
   title: 'Craft',
   description: 'Crafting interfaces, immersed in art+technology.',
+  authors: [{ name: 'Kelvin Ampofo' }],
   openGraph: {
     title: 'Craft',
     description: 'Crafting interfaces, immersed in art+technology.'
