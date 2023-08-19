04a3cce7fc8ec873ccbdc31361889266dd6110d9
Kelvin Ampofo <kelvinamp@outlook.com>
2023-08-19 12:30:48 +0100

    chore: update sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,20 +1,26 @@
 import { allPosts } from 'contentlayer/generated';
 import { MetadataRoute } from 'next';
+import { allPrototypes } from './data/prototypes';
 
-function formatDate(date: Date): string {
+function formatDate(date: Date) {
   return date.toISOString().split('T')[0];
 }
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const posts = allPosts.map((post) => ({
-    url: `https://kelvinamp.me/thoughts${post.slug}`,
+  const posts = allPosts.map(({ slug }) => ({
+    url: `https://kelvinamp.me/writing/${slug}`,
     lastModified: formatDate(new Date())
   }));
 
-  const routes = ['', '/now', '/thoughts'].map((route) => ({
+  const prototypes = allPrototypes.map(({ slug }) => ({
+    url: `https://kelvinamp.me/craft${slug}`,
+    lastModified: formatDate(new Date())
+  }));
+
+  const routes = ['', '/now', '/writing', '/craft'].map((route) => ({
     url: `https://kelvinamp.me${route}`,
     lastModified: formatDate(new Date())
   }));
 
-  return [...routes, ...posts];
+  return [...routes, ...posts, ...prototypes];
 }
