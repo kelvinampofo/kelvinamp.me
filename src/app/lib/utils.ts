e9778b1366d414b3f8f8850a459f607e94ad527e
Kelvin Ampofo <kelvinamp@outlook.com>
2023-09-09 18:37:39 +0100

    feat: add error message util

--- src/app/lib/utils.ts
+++ src/app/lib/utils.ts
@@ -7,3 +7,22 @@ export const isWithin1Month = (postDate: Date) => {
     end: currentDate
   });
 };
+
+export const getErrorMessage = (error: unknown) => {
+  if (error instanceof Error) {
+    return error.message;
+  }
+
+  if (typeof error === 'string') {
+    return error;
+  }
+
+  const hasValidMessage =
+    error && typeof error === 'object' && 'message' in error && typeof error.message === 'string';
+
+  if (hasValidMessage) {
+    return error.message as string;
+  }
+
+  return 'Something went wrong.';
+};
