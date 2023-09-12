7a321ec9c4b29cdeac6e2b408473ae6be8469a1f
Kelvin Ampofo <kelvinamp@outlook.com>
2023-09-13 00:36:31 +0100

    chore: refactor

--- src/app/components/ui/CustomLink.tsx
+++ src/app/components/ui/CustomLink.tsx
@@ -7,8 +7,8 @@ export interface CustomLinkProps {
   children?: React.ReactNode;
   ariaLabel?: string;
   arrowIcon?: boolean;
-  resetIcon?: boolean;
-  underline?: boolean;
+  hideUnderline?: boolean;
+  className?: string;
 }
 
 export default function CustomLink({
@@ -16,14 +16,18 @@ export default function CustomLink({
   children,
   ariaLabel,
   arrowIcon,
-  resetIcon,
-  underline
+  hideUnderline,
+  className
 }: CustomLinkProps) {
   const isInternalLink = href.startsWith('/');
 
+  if (children === 'Back') {
+    hideUnderline = true;
+  }
+
   const classes = clsx(
-    (arrowIcon || resetIcon) && 'inline-flex items-center',
-    underline &&
+    className,
+    !hideUnderline &&
       'underline decoration-neutral-300 decoration-1 underline-offset-[2.5px] dark:decoration-[#505050]',
     'duration-150 ease-linear hover:text-[#6F6F6F] dark:hover:text-neutral-400'
   );
@@ -33,7 +37,7 @@ export default function CustomLink({
       <Link href={href} className={classes} aria-label={ariaLabel}>
         {arrowIcon && (
           <ArrowTopLeftIcon
-            className="mr-1"
+            className="mr-1 inline-flex items-center"
             aria-label="Arrow Top Left icon"
             width={18}
             height={18}
