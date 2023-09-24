015c097594367cda5b9dfaf3b2bbec3ac7d3b65f
Kelvin Ampofo <k@kelvinamp.me>
2023-09-24 22:00:43 +0100

    chore: refactor

--- src/app/components/generic/Heading.tsx
+++ src/app/components/generic/Heading.tsx
@@ -5,10 +5,13 @@ interface HeadingProps extends HTMLProps<HTMLHeadingElement> {
   as?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
 }
 
-export default function Heading({ children, className, as = 'h1', ...rest }: HeadingProps) {
-  const HeadingTag = as;
-
-  const fontSizeClass = as === 'h1' ? 'text-lg' : 'text-base';
+export default function Heading({
+  children,
+  className,
+  as: HeadingTag = 'h1',
+  ...rest
+}: HeadingProps) {
+  const fontSizeClass = HeadingTag === 'h1' ? 'text-lg' : 'text-base';
 
   return (
     <HeadingTag className={c('font-medium', fontSizeClass, className)} {...rest}>
