45a4a1d49cc65e22fb64c931d3febda50db62a7a
Kelvin Ampofo <k@kelvinamp.me>
2023-09-27 21:55:11 +0100

    feat: add useTime hook

---
+++ src/app/hooks/useTime.tsx
@@ -0,0 +1,33 @@
+import { format } from 'date-fns';
+import { useEffect, useState } from 'react';
+
+export default function useTime() {
+  const [currentTime, setCurrentTime] = useState(new Date());
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setIsClient(true);
+      const newTime = new Date();
+      if (newTime.getSeconds() !== currentTime.getSeconds()) {
+        setCurrentTime(newTime);
+      }
+    }, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, [currentTime]);
+
+  const formattedTime = format(currentTime, 'HH:mm:ss');
+  const period = currentTime.getHours() >= 12 ? 'PM' : 'AM';
+
+  if (isClient) {
+    return (
+      <div className="flex">
+        <span className="w-14 md:w-16">{formattedTime}</span>
+        <span>{period}</span>
+      </div>
+    );
+  }
+}
