cf2edde9659842c88e44de04d65cfc8c111bca25
Kelvin Ampofo <k@kelvinamp.me>
2023-09-27 21:53:10 +0100

    chore: add size prop/classes

--- src/app/components/generic/Text.tsx
+++ src/app/components/generic/Text.tsx
@@ -1,16 +1,19 @@
 import c from 'clsx';
 import { HTMLProps } from 'react';
 
-interface TextProps extends HTMLProps<HTMLParagraphElement | HTMLDivElement | HTMLSpanElement> {
+interface TextProps
+  extends Omit<HTMLProps<HTMLParagraphElement | HTMLDivElement | HTMLSpanElement>, 'size'> {
   as?: 'p' | 'div' | 'span';
   weight?: 'regular' | 'medium' | 'bold';
   colour?: 'primary' | 'secondary';
+  size?: 'small' | 'normal' | 'large';
 }
 
 export default function Text({
   as: Tag = 'p',
   weight = 'regular',
   colour = 'primary',
+  size = 'normal',
   className,
   children
 }: TextProps) {
@@ -21,11 +24,17 @@ export default function Text({
   );
 
   const colourClasses = c(
-    colour === 'primary' && 'text-[#161616]',
-    colour === 'primary' && 'dark:text-[#EDEDED]',
-    colour === 'secondary' && 'text-[#6F6F6F]',
-    colour === 'secondary' && 'dark:text-[#A0A0A0]'
+    colour === 'primary' && 'text-primary',
+    colour === 'primary' && 'dark:text-primary-dark',
+    colour === 'secondary' && 'text-secondary',
+    colour === 'secondary' && 'dark:text-secondary-dark'
   );
 
-  return <Tag className={c(weightClasses, colourClasses, className)}>{children}</Tag>;
+  const sizeClasses = c(
+    size === 'small' && 'text-xs md:text-sm',
+    size === 'normal' && 'md:text-base',
+    size === 'large' && 'md:text-lg'
+  );
+
+  return <Tag className={c(weightClasses, colourClasses, sizeClasses, className)}>{children}</Tag>;
 }
