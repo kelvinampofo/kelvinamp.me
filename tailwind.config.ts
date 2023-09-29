5632e6d7fe6fc33fbc137790f8aa504018fd0417
Kelvin Ampofo <k@kelvinamp.me>
2023-09-29 22:01:08 +0100

    chore: clean up

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,15 +6,13 @@ export default {
     extend: {
       colors: {
         primary: '#161616',
-        'primary-dark': '#EDEDED',
         secondary: '#6F6F6F',
+        'primary-dark': '#EDEDED',
         'secondary-dark': '#A0A0A0'
       },
       spacing: {
         'page-top': 'var(--page-top)',
-        'page-top-mobile': 'var(--page-top-mobile)',
-        'page-bottom': 'var(--page-bottom)',
-        'page-bottom-mobile': 'var(--page-bottom-mobile)'
+        'page-top-mobile': 'var(--page-top-mobile)'
       },
       fontFamily: {
         sans: ['var(--font-inter)']
