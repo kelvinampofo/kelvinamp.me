c4b585726b3b424649dd7f68484ce9777d410914
Kelvin Ampofo <k@kelvinamp.me>
2023-10-01 21:23:24 +0100

    chore: adjust width, again

--- src/app/hooks/useTime.tsx
+++ src/app/hooks/useTime.tsx
@@ -3,11 +3,9 @@ import { useEffect, useState } from 'react';
 
 export default function useTime() {
   const [currentTime, setCurrentTime] = useState(new Date());
-  const [isClient, setIsClient] = useState(false);
 
   useEffect(() => {
     const timer = setInterval(() => {
-      setIsClient(true);
       const newTime = new Date();
       if (newTime.getSeconds() !== currentTime.getSeconds()) {
         setCurrentTime(newTime);
@@ -20,14 +18,12 @@ export default function useTime() {
   }, [currentTime]);
 
   const formattedTime = format(currentTime, 'HH:mm:ss');
-  const period = currentTime.getHours() >= 12 ? 'PM' : 'AM';
+  const timePeriod = currentTime.getHours() >= 12 ? 'PM' : 'AM';
 
-  if (isClient) {
-    return (
-      <div className="flex">
-        <span className="w-[3.5rem] md:w-[4rem]">{formattedTime}</span>
-        <span>{period}</span>
-      </div>
-    );
-  }
+  return (
+    <>
+      <span className="w-[3.2rem] md:w-[3.8rem]">{formattedTime}</span>
+      <span>{timePeriod}</span>
+    </>
+  );
 }
