ffe22e85404fe89b8d5afecb19c1ac824265502c
Kelvin Ampofo <k@kelvinamp.me>
2023-10-07 13:23:07 +0100

    chore: refactor

--- src/app/components/generic/AnimateEnter.tsx
+++ src/app/components/generic/AnimateEnter.tsx
@@ -1,12 +1,12 @@
 import { LazyMotion, domAnimation, m, useReducedMotion } from 'framer-motion';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, type ReactNode } from 'react';
 
-interface PageWrapperProps {
-  children: React.ReactNode;
+interface AnimateEnterProps {
+  children: ReactNode;
   delay?: number;
 }
 
-export default function AnimateEnter({ children, delay = 0 }: PageWrapperProps) {
+export default function AnimateEnter({ children, delay = 0 }: AnimateEnterProps) {
   const [isClient, setIsClient] = useState(false);
   const prefersReducedMotion = useReducedMotion();
 
@@ -14,22 +14,18 @@ export default function AnimateEnter({ children, delay = 0 }: PageWrapperProps) 
     setIsClient(true);
   }, []);
 
-  if (isClient) {
-    if (prefersReducedMotion) {
-      return <>{children}</>;
-    }
-
-    return (
-      <LazyMotion features={domAnimation}>
-        <m.div
-          initial={{ opacity: 0, y: 20 }}
-          animate={{ opacity: 1, y: 0 }}
-          exit={{ opacity: 0, y: 20 }}
-          transition={{ ease: 'linear', duration: 0.35, delay }}
-        >
-          {children}
-        </m.div>
-      </LazyMotion>
-    );
-  }
+  return isClient && prefersReducedMotion ? (
+    children
+  ) : (
+    <LazyMotion features={domAnimation}>
+      <m.div
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: 20 }}
+        transition={{ ease: 'linear', duration: 0.35, delay }}
+      >
+        {children}
+      </m.div>
+    </LazyMotion>
+  );
 }
