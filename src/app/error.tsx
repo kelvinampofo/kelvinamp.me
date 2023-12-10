6f6f2cb4c5f0883a62b17d20306d2bb627f22f90
Kelvin Ampofo <k@kelvinamp.me>
2023-12-10 20:09:21 +0000

    refactor: update error boundary handling + aesthetics.

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,19 +1,61 @@
 'use client';
 
 import Container from '@/app/components/generic/Container';
+import Heading from './components/generic/Heading';
+import Text from './components/generic/Text';
+import { Icon } from './components/ui/Icon';
 
 interface ErrorBoundaryProps {
   error: Error;
   reset: () => void;
 }
 
-export default function ErrorBoundary({ error }: ErrorBoundaryProps) {
-  console.error(error);
+export default function ErrorBoundary({ error, reset }: ErrorBoundaryProps) {
+  const isDevelopmentMode = process.env.NODE_ENV === 'development';
 
   return (
     <Container>
-      <h1 className="mb-6 text-lg font-medium">Oops something went wrong...</h1>
-      <p>Very much sad, very high priority. Maybe refresh?</p>
+      <Heading className="mb-6 text-lg font-medium">Oops, something went wrong...</Heading>
+      <div className="flex flex-col items-start gap-3">
+        <Text>Very much sad, very high priority. Maybe refresh?</Text>
+        <button
+          onClick={reset}
+          className="rounded-md border bg-neutral-50 p-1 shadow-sm dark:border-neutral-800 dark:bg-[#1C1C1C]"
+        >
+          Refresh
+          <Icon
+            name="reload"
+            height={15}
+            width={15}
+            className="mx-1 inline-flex items-center text-secondary"
+          />
+        </button>
+        <details className="dark:border-neutral-800 [&_svg]:open:rotate-90">
+          <summary className="mb-3 flex cursor-pointer list-none items-center gap-1">
+            <div>
+              <Icon
+                name="chevron-right"
+                className="rotate-[15] transition-all duration-300 dark:text-secondary-dark"
+              />
+            </div>
+            <div>Stats for nerds</div>
+            <Icon name="rocket" className="mx-1 text-secondary" />
+          </summary>
+          <div className="flex flex-col gap-1">
+            <p>
+              <strong>Name:</strong> {error.name}
+            </p>
+            <p>
+              <strong>Message:</strong> {error.message}
+            </p>
+            {isDevelopmentMode && (
+              <p>
+                <strong>Stack:</strong> {error.stack}
+              </p>
+            )}
+          </div>
+        </details>
+      </div>
     </Container>
   );
 }
