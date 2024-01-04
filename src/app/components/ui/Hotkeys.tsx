d43baaa0afb173a45761f102f7a4252cbfb7b75b
Kelvin Ampofo <k@kelvinamp.me>
2024-01-04 22:12:09 +0000

    chore: refine shadows on hotkeys

--- src/app/components/ui/Hotkeys.tsx
+++ src/app/components/ui/Hotkeys.tsx
@@ -8,7 +8,7 @@ export default function Hotkeys({ keys }: HotkeysProps) {
       {keys.map((key, index) => (
         <span
           key={index}
-          className="rounded-sm border px-1 shadow-sm dark:border-neutral-800 dark:bg-neutral-800 dark:shadow-black"
+          className="rounded-sm border px-1 shadow-sm shadow-neutral-200 dark:border-neutral-800 dark:bg-neutral-800 dark:shadow-neutral-950"
         >
           {key}
         </span>
