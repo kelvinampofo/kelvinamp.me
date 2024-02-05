a4d134e1d90422eebb3e852202b9aee3a7f43a0d
Kelvin Ampofo <k@kelvinamp.me>
2024-02-05 20:35:34 +0000

    feat: add alternate video to prototype

--- src/app/(pages)/craft/focused-reading/FocusedReadingPrototype.tsx
+++ src/app/(pages)/craft/focused-reading/FocusedReadingPrototype.tsx
@@ -14,14 +14,10 @@ export default function FocusedReadingPrototype() {
       {!isPointerDevice && (
         <div className="flex flex-col gap-3">
           <Callout color="amber">This protype requires a pointer device.</Callout>
-          <Card className="flex h-52 items-center justify-center gap-12 md:gap-20">
-            <FocusedReading>
-              <div className="flex flex-col gap-2 p-1">
-                <p>Lorem ipsum dolor sit amet.</p>
-                <p>Lorem ipsum dolor sit amet consectetur.</p>
-                <p>Lorem ipsum dolor sit amet.</p>
-              </div>
-            </FocusedReading>
+          <Card className="m-2 flex h-52 items-center justify-center">
+            <video autoPlay playsInline className="h-auto w-auto rounded-sm">
+              <source src="/assets/videos/focused-reading.mp4" type="video/mp4" />
+            </video>
           </Card>
         </div>
       )}
