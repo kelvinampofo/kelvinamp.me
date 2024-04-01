ce77e62e49291374c84ea950b8bf3be52132e9a5
Kelvin Ampofo <k@kelvinamp.me>
2024-04-01 19:49:25 +0100

    chore: more spring cleaning

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -60,7 +60,10 @@ export const metadata: Metadata = {
   }
 };
 
-const newsreader = Newsreader({ subsets: ['latin'], variable: '--font-newsreader' });
+const newsreader = Newsreader({
+  subsets: ['latin'],
+  variable: '--font-newsreader'
+});
 
 export interface RootLayoutProps {
   children: ReactNode;
@@ -74,7 +77,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
     >
       <body className="text-sm text-primary antialiased dark:bg-primary dark:text-primary-dark md:text-base lg:text-base">
         <Providers>
-          <main className="container mx-auto min-h-full max-w-3xl pb-page-bottom-mobile pt-page-top-mobile md:pb-page-bottom md:pt-page-top">
+          <main className="mx-auto min-h-screen max-w-3xl pb-page-bottom-mobile pt-page-top-mobile md:pb-page-bottom md:pt-page-top">
             {children}
             <Analytics />
           </main>
