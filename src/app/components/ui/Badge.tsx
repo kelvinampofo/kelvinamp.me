acf059d9b8f5443a49c772736b95008487e5e68b
Kelvin Ampofo <k@kelvinamp.me>
2024-05-26 13:55:21 +0100

    feat: lil polish

--- src/app/components/ui/Badge.tsx
+++ src/app/components/ui/Badge.tsx
@@ -7,7 +7,7 @@ export default function Badge({ children, ariaHidden = false }: BadgeProps) {
   return (
     <span
       aria-hidden={ariaHidden}
-      className="dark:shine-animation text-xs text-blue-600 dark:bg-gradient-to-r dark:from-teal-200 dark:via-blue-600 dark:to-teal-200 dark:bg-clip-text dark:bg-left dark:text-transparent"
+      className="dark:shine bg-[length:200%_100%] text-xs text-blue-600 dark:bg-gradient-to-r dark:from-blue-600 dark:via-teal-300 dark:to-blue-600 dark:bg-clip-text dark:text-transparent"
     >
       {children}
     </span>
