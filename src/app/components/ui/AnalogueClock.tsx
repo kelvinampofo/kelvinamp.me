08bc07a3e8c914ca8d32601432838c4491a3b16e
Kelvin Ampofo <k@kelvinamp.me>
2024-06-27 22:18:28 +0000

    feat(analogueclock): add second angle + lil polish

--- src/app/components/ui/AnalogueClock.tsx
+++ src/app/components/ui/AnalogueClock.tsx
@@ -5,23 +5,30 @@ import { useTime } from '@/app/hooks/useTime';
 export default function AnalogClock() {
   const { currentTime } = useTime();
 
-  const [hours, minutes] = currentTime.split(':').map((timePart) => parseInt(timePart, 10));
+  const [hours, minutes, seconds] = currentTime.split(':').map((part) => parseInt(part, 10));
 
-  const hourAngle = (hours % 12) * 30 + minutes * 0.5;
-  const minuteAngle = minutes * 6;
+  const hourAngle = (hours % 12) * 30 + minutes * 0.5 + seconds * (0.5 / 60);
+  const minuteAngle = minutes * 6 + seconds * 0.1;
+  const secondAngle = seconds * 6;
 
   return (
-    <div className="relative inline-flex size-5 rounded-full border border-secondary dark:border-neutral-600">
+    <div className="relative inline-flex size-5 rounded-full border border-neutral-400 dark:border-neutral-600">
       <div
-        className="absolute left-1/2 top-[6px] z-10 ml-[-0.25px] h-1 w-px origin-[bottom_center] rounded-t-sm bg-secondary dark:bg-neutral-500"
+        className="absolute inset-x-1/2 top-[6px] z-10 ml-[-1.2px] h-1 w-px origin-[bottom_center] rounded-lg bg-secondary dark:bg-neutral-500"
         style={{
-          transform: `rotateZ(${hourAngle}deg)`
+          transform: `rotate(${hourAngle}deg)`
         }}
       />
       <div
-        className="absolute left-1/2 top-[3px] z-20 ml-[-0.25px] h-[7px] w-px origin-[bottom_center] rounded-t-sm bg-secondary/80 dark:bg-neutral-500"
+        className="absolute left-1/2 top-[3px] z-20 ml-[-1.2px] h-[7px] w-px origin-[bottom_center] rounded-lg bg-secondary dark:bg-neutral-500"
         style={{
-          transform: `rotateZ(${minuteAngle}deg)`
+          transform: `rotate(${minuteAngle}deg)`
+        }}
+      />
+      <div
+        className="absolute left-1/2 top-[2.5px] z-30 -ml-px h-[7.5px] w-[0.5px] origin-[bottom_center] rounded-lg bg-[var(--focus-outer-colour)]"
+        style={{
+          transform: `rotate(${secondAngle}deg)`
         }}
       />
     </div>
