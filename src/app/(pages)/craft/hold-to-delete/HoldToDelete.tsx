d252d9d6044c41abcd1725dc1c460b863fc2d22a
Kelvin Ampofo <k@kelvinamp.me>
2024-07-09 22:32:45 +0000

    feat: update to use clip-path + smol refactor

--- src/app/(pages)/craft/hold-to-delete/HoldToDelete.tsx
+++ src/app/(pages)/craft/hold-to-delete/HoldToDelete.tsx
@@ -6,7 +6,6 @@ import { Icon } from '../../../components/ui/Icon';
 const MAX_PROGRESS = 100;
 const INTERVAL_MS = 100;
 const DURATION = 1600; // ms
-
 const INCREMENT = MAX_PROGRESS / (DURATION / INTERVAL_MS);
 
 export default function HoldToDelete() {
@@ -20,50 +19,48 @@ export default function HoldToDelete() {
     }
   };
 
-  // increment the progress state over time until the maximum progress is reached.
-  const handleMouseDown = () => {
+  const startProgress = () => {
     if (!intervalIdRef.current) {
       intervalIdRef.current = setInterval(() => {
-        setProgress((prev) => {
-          const newProgress = prev + INCREMENT;
+        setProgress((prevProgress) => {
+          const newProgress = prevProgress + INCREMENT;
           if (newProgress >= MAX_PROGRESS) {
             clearIntervalId();
 
             return MAX_PROGRESS;
           }
-
           return newProgress;
         });
       }, INTERVAL_MS);
     }
   };
 
-  const handleMouseUp = () => {
+  const resetProgress = () => {
     clearIntervalId();
     setProgress(0);
   };
 
+  const handleMouseDown = () => startProgress();
+  const handleMouseUp = () => resetProgress();
   const handleKeyDown = (event: React.KeyboardEvent) => {
     if (event.key === 'Enter' || event.key === ' ') {
-      handleMouseDown();
+      startProgress();
     }
   };
 
   const handleKeyUp = (event: React.KeyboardEvent) => {
     if (event.key === 'Enter' || event.key === ' ') {
-      handleMouseUp();
+      resetProgress();
     }
   };
 
   useEffect(() => {
-    if (intervalIdRef.current) {
-      return clearInterval(intervalIdRef.current);
-    }
+    return () => clearIntervalId();
   }, []);
 
   return (
     <button
-      className="relative flex items-center justify-center overflow-x-hidden rounded-md border bg-neutral-50 px-4 py-2 transition-all hover:border-neutral-200 hover:bg-neutral-100 active:scale-[.98] dark:border-neutral-800 dark:bg-[#1C1C1C] hover:dark:border-neutral-700 hover:dark:bg-neutral-800"
+      className="relative flex items-center justify-center overflow-hidden rounded-md border bg-neutral-50 px-4 py-2 transition-all hover:border-neutral-200 hover:bg-neutral-100 active:scale-[.98] dark:border-neutral-800 dark:bg-[#1C1C1C] hover:dark:border-neutral-700 hover:dark:bg-neutral-800"
       onMouseDown={handleMouseDown}
       onMouseUp={handleMouseUp}
       onMouseLeave={handleMouseUp}
@@ -71,16 +68,16 @@ export default function HoldToDelete() {
       onTouchEnd={handleMouseUp}
       onKeyDown={handleKeyDown}
       onKeyUp={handleKeyUp}
-      style={{
-        userSelect: 'none',
-        WebkitUserSelect: 'none'
-      }}
+      style={{ userSelect: 'none', WebkitUserSelect: 'none' }}
       aria-label="hold to delete"
     >
       <div
         aria-hidden="true"
-        style={{ width: `${progress}%`, transition: 'width 0.2s linear' }}
-        className="absolute left-0 h-full bg-red-500/80"
+        style={{
+          clipPath: `polygon(0 0, ${progress}% 0, ${progress}% 100%, 0 100%)`,
+          transition: 'clip-path 0.2s linear'
+        }}
+        className="absolute inset-0 bg-red-500/80"
       />
       <div className="flex items-center gap-2 text-primary dark:text-primary-dark">
         <Icon
