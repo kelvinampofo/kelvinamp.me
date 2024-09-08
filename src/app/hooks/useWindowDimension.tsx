57ef232a279917a0b5df7169fe3e0cf25deeb7a3
Kelvin Ampofo <k@kelvinamp.me>
2024-09-08 21:30:19 +0100

    feat: add window dimension hook

---
+++ src/app/hooks/useWindowDimension.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
+export const useWindowDimension = () => {
+  const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
+
+  useEffect(() => {
+    const updateDimensions = () => {
+      setDimensions({
+        width: window.innerWidth,
+        height: window.innerHeight
+      });
+    };
+
+    updateDimensions();
+
+    window.addEventListener('resize', updateDimensions);
+
+    return () => {
+      window.removeEventListener('resize', updateDimensions);
+    };
+  }, []);
+
+  return dimensions;
+};
