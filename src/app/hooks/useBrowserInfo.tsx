0dcebc7b7fdc47baf0f32c52966d3b9b0d2921a3
Kelvin Ampofo <k@kelvinamp.me>
2024-09-08 22:06:33 +0100

    refactor: update browser info hook logic

--- src/app/hooks/useBrowserInfo.tsx
+++ src/app/hooks/useBrowserInfo.tsx
@@ -1,36 +1,31 @@
-'use client';
+import { useMemo } from 'react';
 
-import { useEffect, useState } from 'react';
+interface BrowserInfo {
+  name: string;
+  version: string;
+}
 
 export const useBrowserInfo = () => {
-  const [browserInfo, setBrowserInfo] = useState({ name: 'Unknown', version: '0.0' });
+  const browserInfo = useMemo<BrowserInfo>(() => {
+    const userAgent = navigator.userAgent;
 
-  useEffect(() => {
-    const getBrowserInfo = () => {
-      const userAgent = navigator.userAgent;
+    const browsers = [
+      { name: 'Chrome', regex: /Chrome\/(\d+\.\d+)/ },
+      { name: 'Firefox', regex: /Firefox\/(\d+\.\d+)/ },
+      { name: 'Safari', regex: /Version\/(\d+\.\d+)/ }
+    ];
 
-      const browserNameAndRegex = [
-        { name: 'Chrome', regex: /Chrome\/(\d+\.\d+)/ },
-        { name: 'Firefox', regex: /Firefox\/(\d+\.\d+)/ },
-        { name: 'Safari', regex: /Version\/(\d+\.\d+)/ }
-      ];
+    const matchedBrowser = browsers.find(({ regex }) => regex.test(userAgent));
 
-      const matchedBrowser = browserNameAndRegex.find(({ regex }) => regex.test(userAgent));
-
-      if (matchedBrowser) {
-        const match = userAgent.match(matchedBrowser.regex);
-        if (match) {
-          const [major, minor] = match[1].split('.');
-          return { name: matchedBrowser.name, version: `${major}.${minor}` };
-        }
+    if (matchedBrowser) {
+      const match = userAgent.match(matchedBrowser.regex);
+      if (match) {
+        const [major, minor] = match[1].split('.');
+        return { name: matchedBrowser.name, version: `${major}.${minor}` };
       }
+    }
 
-      return { name: 'Unknown', version: '0.0' };
-    };
-
-    const { name, version } = getBrowserInfo();
-
-    setBrowserInfo({ name, version });
+    return { name: 'Unknown', version: '0.0' };
   }, []);
 
   return browserInfo;
