310d6935e3a35aeaba2487df89b2d27131ed4c90
Kelvin Ampofo <k@kelvinamp.me>
2024-09-08 09:29:26 +0100

    feat: add keyboard Input element

--- src/app/components/ui/ShortcutKey.tsx
+++ src/app/components/ui/ShortcutKey.tsx
@@ -4,12 +4,12 @@ export default function ShortcutKeys({ keyShortcuts }: { keyShortcuts: string[] 
   return (
     <div className="flex gap-1 text-secondary-dark" aria-keyshortcuts={keyShortcuts.join('+')}>
       {keyShortcuts.map((keyShortcut, index) => (
-        <span
+        <kbd
           key={index}
           className="rounded-sm bg-neutral-100 px-1 text-xs text-secondary dark:bg-neutral-800 dark:text-secondary-dark"
         >
           {keyShortcut}
-        </span>
+        </kbd>
       ))}
     </div>
   );
