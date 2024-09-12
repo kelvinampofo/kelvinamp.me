f6270ef922a2d1b965648be1c9b81d1826e6748d
Kelvin Ampofo <k@kelvinamp.me>
2024-09-12 23:08:04 +0100

    refactor: keep it simple stupid

--- src/app/hooks/useClipboard.ts
+++ src/app/hooks/useClipboard.ts
@@ -1,12 +1,10 @@
 import { parseError } from '@/app/lib/utils';
-import { useCallback, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export function useClipboard() {
   const [isCopied, setIsCopied] = useState(false);
   const [isError, setIsError] = useState(false);
   const [error, setError] = useState({ message: '' });
-
-  const currentUrl = typeof window !== 'undefined' ? window.location.href : '';
 
   useEffect(() => {
     if (isCopied) {
@@ -15,43 +13,27 @@ export function useClipboard() {
     }
   }, [isCopied]);
 
-  const handleCopy = useCallback(async () => {
-    try {
-      if (navigator.clipboard && navigator.clipboard.write) {
-        const mimeType = 'text/plain';
+  const currentUrl = typeof window !== 'undefined' ? window.location.href : '';
 
-        const clipboardItem = new ClipboardItem({
-          mimeType: new Promise(async (resolve) => {
-            try {
-              const copyText = currentUrl || ''; // fallback to empty string if currentUrl is falsy
-
-              // if the result is valid, resolve with a Blob containing the text
-              resolve(new Blob([copyText], { type: mimeType }));
-            } catch (error) {
-              resolve(new Blob([''], { type: mimeType }));
-            }
-          })
-        });
-
-        await navigator.clipboard.write([clipboardItem]);
-      } else {
-        // fallback for browsers that don't support clipboard.write
+  const copyUrl = async () => {
+    if (navigator.clipboard) {
+      try {
         await navigator.clipboard.writeText(currentUrl);
+        setIsCopied(true);
+        setIsError(false);
+        setError({ message: '' });
+      } catch (error) {
+        setError({ message: parseError(error) });
+        setIsError(true);
+        setIsCopied(false);
       }
-      setIsCopied(true);
-      setIsError(false);
-      setError({ message: '' });
-    } catch (error) {
-      setError({ message: parseError(error) });
-      setIsError(true);
-      setIsCopied(false);
     }
-  }, [currentUrl]);
+  };
 
   return {
     isCopied,
     isError,
     error,
-    copyUrl: handleCopy
+    copyUrl
   };
 }
