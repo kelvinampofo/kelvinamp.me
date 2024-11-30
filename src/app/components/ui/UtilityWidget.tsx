f25b16b5ec215ab5ea401d951a669b5c4d86f2cb
Kelvin Ampofo <kelvinamp@outlook.com>
2024-11-30 11:53:30 +0000

    refactor: optimise UtilityWidget component for readability and performance
    - corrected `aria-live` attribute to improve accessibility
    - simplified view handling logic

--- src/app/components/ui/UtilityWidget.tsx
+++ src/app/components/ui/UtilityWidget.tsx
@@ -4,7 +4,7 @@ import { useBrowserInfo } from '@/app/hooks/useBrowserInfo';
 import { useTime } from '@/app/hooks/useTime';
 import { useWindowDimension } from '@/app/hooks/useWindowDimension';
 import { format } from 'date-fns';
-import React, { useState } from 'react';
+import { useState } from 'react';
 import AnalogueClock from './AnalogueClock';
 import Tooltip from './Tooltip';
 
@@ -23,42 +23,37 @@ export default function UtilityWidget() {
   const { name, version } = useBrowserInfo();
   const { width, height } = useWindowDimension();
 
-  // dividing by 2 because enums create both string and number keys
   const totalViews = Object.keys(Views).length / 2;
 
-  const handleView = () => setViewIndex((prevIndex) => (prevIndex + 1) % totalViews);
-
-  const renderView = () => {
-    switch (viewIndex) {
-      case Views.Time:
-        return currentTime;
-      case Views.Clock:
-        return <AnalogueClock />;
-      case Views.Dimensions:
-        return `${width}x${height}`;
-      case Views.CurrentDate:
-        return `${format(new Date(), 'EEEE dd MMMM')}`;
-      case Views.BrowserInfo:
-        return `${name} ${version}`;
-      default:
-        return null;
-    }
+  const content = {
+    [Views.Time]: currentTime,
+    [Views.Clock]: <AnalogueClock />,
+    [Views.Dimensions]: `${width}x${height}`,
+    [Views.CurrentDate]: format(new Date(), 'EEEE dd MMMM'),
+    [Views.BrowserInfo]: `${name} ${version}`
   };
+
+  const handleView = () =>
+    setViewIndex((prevIndex) => (prevIndex + 1) % totalViews);
 
   const children = (
     <span
       className="inline-flex cursor-default gap-0.5 text-[10px] tabular-nums tracking-wider text-secondary dark:text-secondary-dark sm:text-xs"
-      aria-live="off"
+      aria-live="polite"
       role="status"
       onMouseDown={handleView}
     >
-      {renderView()}
+      {content[viewIndex]}
     </span>
   );
 
   return (
     <div className="mt-12">
-      {viewIndex === Views.Time ? <Tooltip content={timezoneOffset}>{children}</Tooltip> : children}
+      {viewIndex === Views.Time ? (
+        <Tooltip content={timezoneOffset}>{children}</Tooltip>
+      ) : (
+        children
+      )}
     </div>
   );
 }
