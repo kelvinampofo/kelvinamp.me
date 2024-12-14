fde252c9de706ae55be8805a0519497bc30ac3fa
Kelvin Ampofo <kelvinamp@outlook.com>
2024-12-14 13:45:26 +0000

    chore: update references to use global CSS animations

--- src/app/components/ui/Badge.tsx
+++ src/app/components/ui/Badge.tsx
@@ -7,7 +7,7 @@ export default function Badge({ children, ariaHidden = false }: BadgeProps) {
   return (
     <span
       aria-hidden={ariaHidden}
-      className="bg-[length:200%] text-xs text-blue-600 dark:animate-shine dark:bg-gradient-to-r dark:from-blue-600 dark:via-teal-300 dark:to-blue-600 dark:bg-clip-text dark:text-transparent"
+      className="dark:shimmer bg-200% text-xs text-blue-600 dark:bg-gradient-to-r dark:from-blue-600 dark:via-teal-300 dark:to-blue-600 dark:bg-clip-text dark:text-transparent"
     >
       {children}
     </span>
