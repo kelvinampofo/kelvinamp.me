2eb4ace530010ebfed2c7270977554bebc949304
Kelvin Ampofo <kelvinamp@outlook.com>
2025-01-18 02:59:37 +0000

    refactor: Improve AnalogClock time computation, composition, and accessibility

--- src/app/components/ui/AnalogueClock.tsx
+++ src/app/components/ui/AnalogueClock.tsx
@@ -1,39 +1,67 @@
 'use client';
 
 import { useTime } from '@/app/hooks/useTime';
+import c from 'clsx';
 
 export default function AnalogClock() {
   const { currentTime } = useTime();
 
   const [hours, minutes, seconds] = currentTime.split(':').map((part) => parseInt(part, 10));
 
-  const hoursDeg = (hours / 12) * 360 + (minutes / 60) * 30;
-  const minutesDeg = (minutes / 60) * 360 + (seconds / 60) * 6;
-  const secondsDeg = (seconds / 60) * 360;
+  const hoursDeg = (hours % 12) * 30 + (minutes / 60) * 30;
+  const minutesDeg = minutes * 6 + (seconds / 60) * 6;
+  const secondsDeg = seconds * 6;
 
   return (
     <div
       className="relative inline-flex size-5 rounded-full border border-neutral-400 dark:border-neutral-600"
-      aria-hidden="true"
+      aria-label="Analog clock showing the current time"
+      role="img"
     >
-      <div
-        className="absolute inset-x-1/2 top-[6px] z-10 ml-[-1.2px] h-1 w-px origin-[bottom_center] rounded-lg bg-secondary dark:bg-neutral-500"
-        style={{
-          transform: `rotate(${hoursDeg}deg)`
-        }}
+      <ClockHand
+        rotation={hoursDeg}
+        height="25%"
+        zIndex={10}
+        color="bg-secondary dark:bg-neutral-500"
       />
-      <div
-        className="absolute left-1/2 top-[3px] z-20 ml-[-1.2px] h-[7px] w-px origin-[bottom_center] rounded-lg bg-secondary dark:bg-neutral-500"
-        style={{
-          transform: `rotate(${minutesDeg}deg)`
-        }}
+
+      <ClockHand
+        rotation={minutesDeg}
+        height="35%"
+        zIndex={20}
+        color="bg-secondary dark:bg-neutral-500"
       />
-      <div
-        className="absolute left-1/2 top-[2.5px] z-30 -ml-px h-[7.5px] w-[0.5px] origin-[bottom_center] rounded-lg bg-[var(--focus-outer-colour)]"
-        style={{
-          transform: `rotate(${secondsDeg}deg)`
-        }}
+
+      <ClockHand
+        rotation={secondsDeg}
+        height="40%"
+        width="0.5px"
+        zIndex={30}
+        color="bg-[var(--focus-outer-colour)]"
       />
     </div>
+  );
+}
+
+interface ClockHandProps {
+  rotation: number;
+  height: string;
+  zIndex: number;
+  color: string;
+  width?: string;
+}
+
+function ClockHand({ rotation, height, width = '1px', zIndex, color }: ClockHandProps) {
+  return (
+    <div
+      className={c('absolute left-1/2 top-1/2 origin-[bottom_center]', color)}
+      style={{
+        transform: `translate(-50%, -100%) rotate(${rotation}deg)`,
+        height,
+        width,
+        zIndex,
+        borderRadius: '2px'
+      }}
+    />
   );
 }
