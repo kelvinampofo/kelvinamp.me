85591b730ad840521a8201db3db612fe497e9451
Kelvin Ampofo <kelvinamp@outlook.com>
2025-01-27 20:47:32 +0000

    feat: enhance List component layout and improve readability

--- src/app/components/ui/List.tsx
+++ src/app/components/ui/List.tsx
@@ -17,39 +17,46 @@ export default function List({ items, route, dateFormat = 'dd/MM/yy' }: ListProp
   const isCraftRoute = route === 'craft';
 
   return (
-    <ol>
-      {items
-        .sort((a, b) => compareDesc(parseISO(a.publishedAt), parseISO(b.publishedAt)))
-        .map(({ publishedAt, slug, title, summary }, index) => {
-          const publishedDate = parseISO(publishedAt);
-          const isNewItem = isWithin1Month(publishedDate);
-          return (
-            <li key={title}>
-              <InlineLink
-                href={`/${route}/${slug}`}
-                hideUnderline
-                className="flex justify-between gap-2 p-1 hover:text-secondary dark:hover:text-secondary-dark"
-              >
-                <div className={c('flex gap-2', isNewItem ? 'items-center' : 'items-baseline')}>
-                  <span>{title}</span>
-                  {isCraftRoute && (
-                    <p className="hidden text-xs text-secondary dark:text-secondary-dark md:block">
-                      {summary}
-                    </p>
-                  )}
-                  {isNewItem && <Badge ariaHidden>new</Badge>}
-                </div>
-                <time
-                  dateTime={publishedAt}
-                  className="text-sm text-secondary dark:text-secondary-dark"
+    <>
+      <div className="flex justify-between gap-2 text-sm text-secondary dark:text-secondary-dark">
+        <span>Title</span>
+        <span>{isCraftRoute ? 'Created at' : 'Published at'}</span>
+      </div>
+      <Separator className="mb-2 mt-3" />
+      <ol>
+        {items
+          .sort((a, b) => compareDesc(parseISO(a.publishedAt), parseISO(b.publishedAt)))
+          .map(({ publishedAt, slug, title, summary }, index) => {
+            const publishedDate = parseISO(publishedAt);
+            const isNewItem = isWithin1Month(publishedDate);
+            return (
+              <li key={title}>
+                <InlineLink
+                  href={`/${route}/${slug}`}
+                  hideUnderline
+                  className="flex justify-between gap-2 p-1 hover:text-secondary dark:hover:text-secondary-dark"
                 >
-                  <span className="tabular-nums">{format(publishedDate, dateFormat)}</span>
-                </time>
-              </InlineLink>
-              {index !== items.length - 1 && <Separator className="my-2" />}
-            </li>
-          );
-        })}
-    </ol>
+                  <div className={c('flex gap-2', isNewItem ? 'items-center' : 'items-baseline')}>
+                    <span className="text-sm">{title}</span>
+                    {isCraftRoute && (
+                      <p className="hidden text-xs text-secondary dark:text-secondary-dark md:block">
+                        {summary}
+                      </p>
+                    )}
+                    {isNewItem && <Badge ariaHidden>new</Badge>}
+                  </div>
+                  <time
+                    dateTime={publishedAt}
+                    className="text-sm text-secondary dark:text-secondary-dark"
+                  >
+                    <span className="tabular-nums">{format(publishedDate, dateFormat)}</span>
+                  </time>
+                </InlineLink>
+                {index !== items.length - 1 && <Separator className="my-2" />}
+              </li>
+            );
+          })}
+      </ol>
+    </>
   );
 }
