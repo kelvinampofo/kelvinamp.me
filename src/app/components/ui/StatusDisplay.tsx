65bc8e99085b304d8466fb4ad9a48be4d5fc94bf
Kelvin Ampofo <kelvinamp@outlook.com>
2025-03-10 18:54:35 +0000

    feat: update StatusDisplay

--- src/app/components/ui/StatusDisplay.tsx
+++ src/app/components/ui/StatusDisplay.tsx
@@ -4,34 +4,30 @@ import { useBrowserInfo } from '@/app/hooks/useBrowserInfo';
 import { useTime } from '@/app/hooks/useTime';
 import { useWindowDimension } from '@/app/hooks/useWindowDimension';
 import { useState, ReactNode } from 'react';
+import { format } from 'date-fns';
 import AnalogueClock from './AnalogueClock';
 import Tooltip from './Tooltip';
 
 enum Views {
+  CurrentTime,
   Clock,
   Dimensions,
   BrowserInfo
 }
 
 export default function StatusDisplay() {
-  const [viewIndex, setViewIndex] = useState(Views.Clock);
+  const [viewIndex, setViewIndex] = useState(Views.CurrentTime);
 
   const { currentTime, timezoneOffset } = useTime();
   const { name, version } = useBrowserInfo();
   const { width, height } = useWindowDimension({ debounceDelay: 100 });
 
   const viewContentMap: Record<Views, ReactNode> = {
+    [Views.CurrentTime]: currentTime,
     [Views.Clock]: <AnalogueClock />,
     [Views.Dimensions]: `${width}x${height}`,
     [Views.BrowserInfo]: `${name} ${version}`
   };
-
-  const tooltipContent = (
-    <div className="flex gap-1 text-xs tabular-nums">
-      {currentTime}
-      <span className="text-secondary dark:text-secondary-dark">{timezoneOffset}</span>
-    </div>
-  );
 
   const viewElement = (
     <span
@@ -52,8 +48,20 @@ export default function StatusDisplay() {
 
   return (
     <div className="mt-16">
-      {viewIndex === Views.Clock ? (
-        <Tooltip content={tooltipContent}>{viewElement}</Tooltip>
+      {viewIndex === Views.Clock || viewIndex === Views.CurrentTime ? (
+        <Tooltip
+          content={
+            viewIndex === Views.Clock ? (
+              <span className="tabular-nums">{currentTime}</span>
+            ) : (
+              <span>
+                {format(new Date(), 'dd MMM')}, {timezoneOffset}
+              </span>
+            )
+          }
+        >
+          {viewElement}
+        </Tooltip>
       ) : (
         viewElement
       )}
