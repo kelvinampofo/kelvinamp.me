8ad832f991364ffa18b721a207fd9b5f8d218342
Kelvin Ampofo <kelvinamp@outlook.com>
2025-04-04 15:53:46 +0100

    update bio wording

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
       <AnimateEnter delay={0.2}>
         <section className="mb-7 flex flex-col gap-6">
           <p>
-            Crafting software with a focus on human-computer interaction and aesthetic sensibility.
+            Making software with a focus on human-computer interaction and aesthetic sensibility.
             Insatiable curiosity for <em>novel user interfaces</em>.
           </p>
           <p>
