0d3c8188b0fdcc4da0ad7683d615ad12c9110f2b
Kelvin Ampofo <kelvinamp@outlook.com>
2025-05-05 12:49:32 +0100

    refactor: simplify styling

--- src/app/components/status-display/components/Principle.tsx
+++ src/app/components/status-display/components/Principle.tsx
@@ -4,13 +4,7 @@ export default function Principle() {
   return (
     <Tooltip content="a reason for being">
       Ikigai (
-      <span
-        lang="ja"
-        style={{
-          fontFamily: "'nbintl', 'Hiragino Sans'",
-          letterSpacing: "0.1em",
-        }}
-      >
+      <span lang="ja" style={{ fontWeight: "600" }}>
         生き甲斐
       </span>
       )
