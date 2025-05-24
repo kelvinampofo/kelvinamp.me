6a583bc93512988ddc086442448c6d906f46d16c
Kelvin Ampofo <kelvnamp@gmail.com>
2025-05-24 20:38:15 +0100

    Refactor component for better reusability and clarity

--- src/app/craft/adaptive-interface/components/AdaptiveInterface.tsx
+++ src/app/craft/adaptive-interface/components/AdaptiveInterface.tsx
@@ -24,12 +24,13 @@ export default function AdaptiveInterface() {
     return () => clearTimeout(timer);
   }, []);
 
+  const resetUsage = useCallback(() => setUsageCount(0), []);
+
   useShortcut("D", handleShortcut, {
     modifiers: "Meta",
     preventDefault: true,
   });
 
-  const resetUsage = useCallback(() => setUsageCount(0), []);
   useShortcut("R", resetUsage);
 
   function determineSizeCategory(count: number) {
@@ -111,22 +112,7 @@ function renderContent(sizeCategory: string, isPressed: boolean) {
     case "large":
       return (
         <div className={clsx(baseClass, styles.sizeLarge)}>
-          <svg
-            className={styles.icon}
-            width={20}
-            height={20}
-            aria-label="trash icon"
-            aria-hidden="true"
-            viewBox="0 0 15 15"
-            fill="none"
-          >
-            <path
-              d="M5.5 1C5.22386 1 5 1.22386 5 1.5C5 1.77614 5.22386 2 5.5 2H9.5C9.77614 2 10 1.77614 10 1.5C10 1.22386 9.77614 1 9.5 1H5.5ZM3 3.5C3 3.22386 3.22386 3 3.5 3H5H10H11.5C11.7761 3 12 3.22386 12 3.5C12 3.77614 11.7761 4 11.5 4H11V12C11 12.5523 10.5523 13 10 13H5C4.44772 13 4 12.5523 4 12V4L3.5 4C3.22386 4 3 3.77614 3 3.5ZM5 4H10V12H5V4Z"
-              fill="currentColor"
-              fillRule="evenodd"
-              clipRule="evenodd"
-            />
-          </svg>
+          <TrashIcon />
           <div className={styles.textGroup}>
             <span className={styles.fontMedium}>Delete</span>
             <ShortcutKey keyShortcuts="⌘+D" />
@@ -137,22 +123,7 @@ function renderContent(sizeCategory: string, isPressed: boolean) {
     case "medium":
       return (
         <div className={clsx(baseClass, styles.sizeMedium)}>
-          <svg
-            className={styles.icon}
-            width={20}
-            height={20}
-            aria-label="trash icon"
-            aria-hidden="true"
-            viewBox="0 0 15 15"
-            fill="none"
-          >
-            <path
-              d="M5.5 1C5.22386 1 5 1.22386 5 1.5C5 1.77614 5.22386 2 5.5 2H9.5C9.77614 2 10 1.77614 10 1.5C10 1.22386 9.77614 1 9.5 1H5.5ZM3 3.5C3 3.22386 3.22386 3 3.5 3H5H10H11.5C11.7761 3 12 3.22386 12 3.5C12 3.77614 11.7761 4 11.5 4H11V12C11 12.5523 10.5523 13 10 13H5C4.44772 13 4 12.5523 4 12V4L3.5 4C3.22386 4 3 3.77614 3 3.5ZM5 4H10V12H5V4Z"
-              fill="currentColor"
-              fillRule="evenodd"
-              clipRule="evenodd"
-            />
-          </svg>
+          <TrashIcon />
           <span>Delete</span>
         </div>
       );
@@ -160,26 +131,32 @@ function renderContent(sizeCategory: string, isPressed: boolean) {
     case "small":
       return (
         <div className={clsx(baseClass, styles.sizeSmall)}>
-          <svg
-            className={styles.icon}
-            width={20}
-            height={20}
-            aria-label="trash icon"
-            aria-hidden="true"
-            viewBox="0 0 15 15"
-            fill="none"
-          >
-            <path
-              d="M5.5 1C5.22386 1 5 1.22386 5 1.5C5 1.77614 5.22386 2 5.5 2H9.5C9.77614 2 10 1.77614 10 1.5C10 1.22386 9.77614 1 9.5 1H5.5ZM3 3.5C3 3.22386 3.22386 3 3.5 3H5H10H11.5C11.7761 3 12 3.22386 12 3.5C12 3.77614 11.7761 4 11.5 4H11V12C11 12.5523 10.5523 13 10 13H5C4.44772 13 4 12.5523 4 12V4L3.5 4C3.22386 4 3 3.77614 3 3.5ZM5 4H10V12H5V4Z"
-              fill="currentColor"
-              fillRule="evenodd"
-              clipRule="evenodd"
-            />
-          </svg>
+          <TrashIcon />
         </div>
       );
 
     default:
       return null;
   }
+}
+
+function TrashIcon() {
+  return (
+    <svg
+      className={styles.icon}
+      width={20}
+      height={20}
+      aria-label="trash icon"
+      aria-hidden="true"
+      viewBox="0 0 15 15"
+      fill="none"
+    >
+      <path
+        d="M5.5 1C5.22386 1 5 1.22386 5 1.5C5 1.77614 5.22386 2 5.5 2H9.5C9.77614 2 10 1.77614 10 1.5C10 1.22386 9.77614 1 9.5 1H5.5ZM3 3.5C3 3.22386 3.22386 3 3.5 3H5H10H11.5C11.7761 3 12 3.22386 12 3.5C12 3.77614 11.7761 4 11.5 4H11V12C11 12.5523 10.5523 13 10 13H5C4.44772 13 4 12.5523 4 12V4L3.5 4C3.22386 4 3 3.77614 3 3.5ZM5 4H10V12H5V4Z"
+        fill="currentColor"
+        fillRule="evenodd"
+        clipRule="evenodd"
+      />
+    </svg>
+  );
 }
