aed6bd5fd19f0c60340a0bb6e9255f3b8772de3b
Kelvin Ampofo <kelvnamp@gmail.com>
2025-10-25 23:48:55 +0100

    Collapse arrow path data to prevent hydration mismatch

--- src/components/back-button/BackButton.tsx
+++ src/components/back-button/BackButton.tsx
@@ -7,20 +7,18 @@ type BackButtonProps = React.ComponentPropsWithoutRef<typeof Link>;
 export default function BackButton({ href, ...props }: BackButtonProps) {
   return (
     <Link href={href} className={styles.backButton} {...props}>
-      <ArrowTopLeft />
+      <ArrowTopLeftIcon />
       <em>Back</em>
     </Link>
   );
 }
 
-function ArrowTopLeft() {
+function ArrowTopLeftIcon() {
   return (
     <svg height={15} width={15} aria-hidden fill="none" viewBox="0 0 15 15">
       <path
         fill="currentColor"
-        d="M11.354 11.354a.5.5 0 0 0 0-.707L4.707 
-        4H9a.5.5 0 0 0 0-1H3.5a.5.5 0 0 0-.5.5V9a.5.5 0 0 0 1 
-        0V4.707l6.646 6.647a.5.5 0 0 0 .708 0Z"
+        d="M11.354 11.354a.5.5 0 0 0 0-.707L4.707 4H9a.5.5 0 0 0 0-1H3.5a.5.5 0 0 0-.5.5V9a.5.5 0 0 0 1 0V4.707l6.646 6.647a.5.5 0 0 0 .708 0Z"
       />
     </svg>
   );
