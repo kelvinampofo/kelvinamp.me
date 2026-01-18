c59886b5891b21a33922a273d6f8fb4b049eeb39
Kelvin Ampofo <kelvnamp@gmail.com>
2026-01-18 14:09:51 +0000

    Update now page

--- src/app/now/page.tsx
+++ src/app/now/page.tsx
@@ -1,6 +1,5 @@
 import clsx from "clsx";
 import { Metadata } from "next";
-import Link from "next/link";
 
 import BackButton from "../../components/back-button/BackButton";
 import Heading from "../../components/heading/Heading";
@@ -24,29 +23,13 @@ export default function Now() {
           and consistency.
         </p>
         <p>
-          All I want to do is make software. Interactivity, typography, motion,
-          touch, performance, accessibility, design—an endless canvas for
-          creativity and <em>possibility</em>.
+          All I want to do is make software and <em>be useful</em>.
+          Interactivity, typography, motion, touch, performance, accessibility,
+          design—an endless canvas for creativity and <em>possibility</em>.
         </p>
         <p>
-          Pursuits aside, probably{" "}
-          <a
-            href="https://literal.club/kelvinamp"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            reading
-          </a>
-          ,{" "}
-          <a
-            href="https://music.apple.com/gb/album/wishful-thinking/1812297817"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            listening
-          </a>
-          , updating my <Link href="/mood">mood board</Link>, or playing with
-          the Porsche{" "}
+          Pursuits aside, I’m investing time in fitness, eating right, spending
+          quality time with family and friends, and playing with the Porsche{" "}
           <a
             href="https://porsche-code.com/PTKANXS8"
             target="_blank"
