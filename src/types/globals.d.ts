946747e12022c4bc297176ef83985f096f48f9b7
Kelvin Ampofo <kelvnamp@gmail.com>
2026-05-03 13:41:32 +0100

    Update TypeScript to 6.0

--- src/types/globals.d.ts
+++ src/types/globals.d.ts
@@ -1,5 +1,11 @@
 import "react";
 
+declare module "*.css";
+declare module "*.module.css" {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
+
 declare module "react" {
   interface CSSProperties {
     [key: `--${string}`]: string | number;
