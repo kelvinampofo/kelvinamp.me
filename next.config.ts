3e812d2be8a7f2ae5231d7541993b50925dcfa31
Kelvin Ampofo <kelvnamp@gmail.com>
2026-07-25 14:51:25 +0100

    Enable React Compiler

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   pageExtensions: ["ts", "tsx"],
+  reactCompiler: true,
   images: {
     formats: ["image/avif", "image/webp"],
   },
