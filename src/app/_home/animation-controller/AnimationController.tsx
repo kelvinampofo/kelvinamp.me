48e52f700cfc6f4c7f6ec6c791da649d61e2da67
Kelvin Ampofo <kelvnamp@gmail.com>
2026-09-08 22:52:22 +0100

    Replace animation tracking flag with User Activation API

--- src/app/_home/animation-controller/AnimationController.tsx
+++ src/app/_home/animation-controller/AnimationController.tsx
@@ -6,20 +6,16 @@ interface AnimationControllerProps {
   children: ReactNode;
 }
 
-let hasAnimated = false;
-
 export default function AnimationController({
   children,
 }: AnimationControllerProps) {
-  const shouldAnimate = !hasAnimated;
-
   useLayoutEffect(() => {
+    const shouldAnimate = !navigator.userActivation.hasBeenActive;
+
     document.documentElement.dataset.animationController = shouldAnimate
       ? "true"
       : "false";
-
-    hasAnimated = true;
-  }, [shouldAnimate]);
+  }, []);
 
   return children;
 }
